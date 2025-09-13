21a138279da83dde612463b3bd91b70cd1f36463
PitoyoAidropHunter <pitoyoaidrophunter@gmail.com>
2025-09-13 15:59:49 +0700

    add banner

--- fetchapi/script.js
+++ fetchapi/script.js
@@ -1,7 +1,26 @@
 import getPopularMovies from "./popular.js";
 import getTopRatedMovies from "./toprated.js";
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.mjs";
+
 const ul = document.getElementById("ul");
-const banner = document.getElementById("banner");
+const swiperWrapper = document.getElementById("swiper-wrapper");
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
 window.addEventListener("load", function () {
   const popularMovies = getPopularMovies();
@@ -15,6 +34,7 @@ window.addEventListener("load", function () {
 
       div.className = "card";
       image.src = `https://image.tmdb.org/t/p/w500/${x.poster_path}`;
+      image.alt = x.title;
 
       list.textContent = x.title;
       console.log(x);
@@ -26,12 +46,38 @@ window.addEventListener("load", function () {
   });
 
   topRatedMovies.then((res) => {
-    res.slice(0, 1).map((x) => {
-      const image = document.createElement("img");
+    res.slice(2, 5).map((x) => {
+      const div = document.createElement("div");
+      const filterDark = document.createElement("div");
+      const bannerContainer = document.createElement("div");
+      const content = document.createElement("div");
+      const title = document.createElement("h1");
+      const description = document.createElement("p");
+      const img = document.createElement("img");
 
-      image.src = `https://image.tmdb.org/t/p/w500/${x.backdrop_path}`;
+      // list.classList.toggle(`slide:nth-child(${key})`);
+      div.classList.toggle(`swiper-slide`);
+      content.classList.toggle("content");
+      filterDark.classList.toggle("filter-dark");
+      img.classList.toggle("banner-image");
+      bannerContainer.classList.toggle("banner-container");
 
-      banner.appendChild(image);
+      div.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500/${x.backdrop_path}')`;
+      img.src = `https://image.tmdb.org/t/p/w500/${x.poster_path}`;
+      img.alt = x.title;
+      title.textContent = x.title;
+      description.textContent = x.overview;
+      bannerContainer.appendChild(img);
+      content.appendChild(title);
+      content.appendChild(description);
+      bannerContainer.appendChild(content);
+
+      div.appendChild(bannerContainer);
+      div.appendChild(filterDark);
+
+      console.log(div);
+
+      swiperWrapper.appendChild(div);
     });
   });
 });
